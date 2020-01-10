4e6beff0cad31fd3d50963fb1d4da5c03c1bac65
Nikolas Santis <nikosantis@gmail.com>
2020-01-10 16:18:31 -0300

    Promises

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,6 +1,6 @@
 const somethingWillHappen = () => {
   return new Promise((resolve, reject) => {
-    if (false) {
+    if (true) {
       resolve('Hey!')
     } else {
       reject('Whooops!')
@@ -27,5 +27,14 @@ const somethingWillHappen2 = () => {
 
 somethingWillHappen2()
   .then(response => console.log(response))
-  .then(() => console.log('Hola'))
-  .catch
+  .then(() => console.log('[OK]'))
+  .catch(err => console.error(err))
+
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(response => {
+    console.log('Array of results', response)
+  })
+  .catch(err => {
+    console.error(err)
+  })
